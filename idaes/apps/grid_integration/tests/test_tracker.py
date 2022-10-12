9ca3f51e5767c81074f8e017cc358050a26b9f7a
dguittet <dguittet@nrel.gov>
2022-10-12 09:20:50 -0600

    merge in latest from Xian's handle-renewable-gen

--- idaes/apps/grid_integration/tests/test_tracker.py
+++ idaes/apps/grid_integration/tests/test_tracker.py
@@ -49,7 +49,9 @@ class TestMissingModel:
             if a != missing_attr:
                 setattr(self, a, self.attr_dict[a])
 
+
 horizon = 4
+
 
 @pytest.mark.unit
 def test_model_object_missing_methods():
@@ -180,3 +182,18 @@ def test_track_market_dispatch(tracker_object):
         pytest.approx(tracker_object.get_last_delivered_power(), abs=1e-3)
         == last_delivered_power
     )
+
+
+@pytest.mark.component
+def test_track_deviation_penalty(tracker_object):
+
+    large_penalty = 10000
+
+    assert pytest.approx(large_penalty) == pyo.value(
+        tracker_object.model.deviation_penalty[0]
+    )
+
+    for t in range(1, horizon):
+        assert pytest.approx(
+            large_penalty / (horizon - tracker_object.n_tracking_hour)
+        ) == pyo.value(tracker_object.model.deviation_penalty[t])
